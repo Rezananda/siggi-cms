2ed9ac3daba63008efd6c1ed5526e9bb4deeeffe
Made Rezananda Putra <maderezanandaputra@172-10-33-6.lightspeed.bcvloh.sbcglobal.net>
2022-09-03 12:14:16 +0700

    add email feature

---
+++ src/api/order/content-types/order/lifecycles.js
@@ -0,0 +1,19 @@
+module.exports = {
+    async afterCreate(event) {    // Connected to "Save" button in admin panel
+        const { result } = event;
+
+        try{
+            await strapi.plugins['email'].services.email.send({
+                to: 'rezananda6897@gmail.com',
+                from: 'siggistore@mail.com', //e.g. single sender verification in SendGrid
+                replyTo: 'siggistore@mail.com',
+                subject: 'The Strapi Email plugin worked successfully',
+                text: 'Hello world!',
+                html: 'Hello world!',  
+            })
+        } catch(err) {
+            console.log(err);
+        }
+    }
+}
+ 
