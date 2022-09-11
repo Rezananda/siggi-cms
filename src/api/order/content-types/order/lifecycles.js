c4ac390c666bc6566985111531ff3bc6a34d904c
Made Rezananda Putra <maderezanandaputra@172-10-33-9.lightspeed.bcvloh.sbcglobal.net>
2022-09-11 19:28:26 +0700

    fixed bugs

--- src/api/order/content-types/order/lifecycles.js
+++ src/api/order/content-types/order/lifecycles.js
@@ -10,6 +10,7 @@ module.exports = {
                 html: `
                 <div style="background-color: white">
                     <h3>Terima kasih sudah berbelanja di siggi.id.</h3>
+                    <p>Berikut adalah ringkasan transaksimu pada ${new Date(order.createdAt).toLocaleTimeString('en-GB')}, ${new Date(order.createdAt).toLocaleDateString('en-GB')}.</p>
                     <div style="margin-bottom: 10px; background-color: #f5f5f4; padding: 10px; border-radius: 5px">
                         <div style="margin-bottom: 10px">
                             <h4 style="margin: 0;">Nama:</h4>
@@ -36,8 +37,9 @@ module.exports = {
                     <div>
                         <h4 style="margin: 0; margin-bottom: 10px">RINGKASAN PEMBAYARAN</h4>
                         <div style="margin-bottom: 10px">
-                            <h4 style="margin: 0;">Total Bayar:</h4>
-                            <p style="margin: 0; color: red; font-weight: bold">Rp. ${result.price_total}</p>
+                            <h4 style="margin: 0;">Total Pesanan :</h4>
+                            <p style="margin: 0; color: red; font-weight: bold">Rp. ${result.price_total}*</p>
+                            <p style="font-size: 0.7rem">*Belum termasuk ongkos kirim</p>
                         </div>
                     </div>
                     <div style="border-bottom: 1px solid; border-color:#d6d3d1; margin-bottom:10px"></div>
@@ -55,10 +57,13 @@ module.exports = {
                             ))}
                         </ul>
                     </div>
-                    <div style="background-color:black; padding: 1rem;">
+                    <div style="background-color:#fcf3eb; padding: 1rem;">
+                        <div style="display: flex; justify-content: right;">
+                            <img src="https://res.cloudinary.com/dz80k02sq/image/upload/v1662888047/siggi_2_0e6e6af858.png?updated_at=2022-09-11T09:20:48.270Z" style="height: 36px; width: 36px; border-radius: 3px"/>
+                        </div>
                         <div>
-                            <p style="color:white; margin: 0;">Ikuti Kami</p>
-                            <div style="display: flex;">
+                            <p style="color:white; margin: 0; text-align:center; color: #b08e5a">Ikuti Kami</p>
+                            <div style="display: flex; justify-content: center">
                                 <a href="https://www.instagram.com/siggi.id/">
                                     <img src="https://img.icons8.com/fluency/36/000000/instagram-new.png" style="height: 36px; width: 36px"/>
                                 </a>
@@ -69,7 +74,7 @@ module.exports = {
                                     <img src="https://img.icons8.com/fluency/36/000000/domain.png" style="height: 36px; width: 36px"/>
                                 </a>
                             </div>
-                            <p style="color: white; justify-content:center; font-size: 10px">Copyright &copy; SIGGI. All Right Reserved</p>
+                            <p style="color: white; justify-content:center; font-size: 10px; text-align:center; color: #b08e5a">Copyright &copy; SIGGI. All Right Reserved</p>
                         </div>
                     </div>
                 </div>
